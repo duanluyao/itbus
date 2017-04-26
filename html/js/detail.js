fc01971b7b22996dbc07b399ea534b5ac23e5463
dubby <yangzheng03@meituan.com>
2017-04-26 14:08:11 +0800

    ...

--- html/js/detail.js
+++ html/js/detail.js
@@ -13,23 +13,29 @@ function freshDetail(busId) {
             var date = new Date();
             date.setTime(data.updateTime);
             $("#title").text(data.busName);
+            $("#busName").text(data.busName);
+
+            $("#createTime").text(date.toLocaleDateString());
+            $("#upCount").text(data.upCount);
+            $("#downCount").text(data.downCount);
+
             $("[name='editormd-markdown-doc']").text(data.busContent);
 
             var testEditormdView = editormd.markdownToHTML("test-editormd-view", {
-                markdown        : data.busContent ,//+ "\r\n" + $("#append-test").text(),
+                markdown: data.busContent,//+ "\r\n" + $("#append-test").text(),
                 //htmlDecode      : true,       // 开启 HTML 标签解析，为了安全性，默认不开启
-                htmlDecode      : "style,script,iframe",  // you can filter tags decode
+                htmlDecode: "style,script,iframe",  // you can filter tags decode
                 //toc             : false,
-                tocm            : true,    // Using [TOCM]
+                tocm: true,    // Using [TOCM]
                 //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
                 //gfm             : false,
                 //tocDropdown     : true,
                 // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
-                emoji           : true,
-                taskList        : true,
-                tex             : true,  // 默认不解析
-                flowChart       : true,  // 默认不解析
-                sequenceDiagram : true  // 默认不解析
+                emoji: true,
+                taskList: true,
+                tex: true,  // 默认不解析
+                flowChart: true,  // 默认不解析
+                sequenceDiagram: true  // 默认不解析
             });
         },
         error: function () {
